8affd579b433d5f231e071ca7f5898a9b95a3269
“Mishka <mishkaismail@icloud.com>
2023-07-30 20:47:32 +0200

    “Update”

---
+++ helloWorld.js
@@ -0,0 +1,5 @@
+// Ask the user for input and store it in a variable
+let userInput = prompt("Please enter 'Hello World!'");
+
+// Print out the inputted data
+console.log("You entered: " + userInput);
