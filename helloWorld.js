b4f770b140aef052075e87b4b6a28828bb5bb7e3
“Mishka <mishkaismail@icloud.com>
2023-08-02 20:57:42 +0200

    Update
    Updated to include Git is awesome

--- helloWorld.js
+++ helloWorld.js
@@ -1,5 +1,5 @@
 // Ask the user for input and store it in a variable
-let userInput = prompt("Please enter 'Hello World'");
+let userInput = prompt("Please enter 'Git is Awesome'");
 
 // Print out the inputted data
 console.log("You entered: " + userInput);
